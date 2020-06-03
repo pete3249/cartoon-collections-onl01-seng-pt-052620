68ae12f1da2fa5b335244bb11f2c97c5d481ddb9
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-03 15:05:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,11 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |planet|
+    plantet.
+  end 
+  array
 end
 
 def long_planeteer_calls# code an argument here
